575b5fd73174066f02f323012b2f9b77f49425ef
kallurianusha <kallurianu13marolix@gmail.com>
2023-09-15 07:49:23 +0530

    first commit

---
+++ file 1.py
@@ -0,0 +1,23 @@
+# # Create a function to add an employee
+# def add_employee():
+#     domain = input("Enter the employee's domain: ")
+#     name = input("Enter the employee's name: ")
+#     empid = input("Enter the employee's ID: ")
+#     email = input("Enter the employee's email: ")
+
+#     employee = {
+#         "domain": domain,
+#         "name": name,
+#         "empid": empid,
+#         "email": email
+#     }
+#     employees.append(employee)
+# def print_employee(employee):
+#     print("Domain:", employee["domain"])
+#     print("Name:", employee["name"])
+#     print("EmpID:", employee["empid"])
+#     print("Email:", employee["email"])
+# employees = []
+# add_employee()
+
+# print_employee(employees[0])
